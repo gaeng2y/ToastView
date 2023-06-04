fbeab55c10c733674400b2b97bff03b405daa072
양경모 <gaeng2y@gmail.com>
2023-06-04 19:40:06 +0900

    error fix

--- ToastView/ToastView/ToastView.swift
+++ ToastView/ToastView/ToastView.swift
@@ -23,7 +23,7 @@ open class ToastView: UIView {
         self.setLayout()
     }
     
-    required init?(coder: NSCoder) {
+    required public init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
